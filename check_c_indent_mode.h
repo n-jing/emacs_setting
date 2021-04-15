dfd2727904cb589726e1c98edcf2d800297389aa
jing <jing_local@163.com>
2021-04-15 11:38:17 +0800

    add indent check file

---
+++ check_c_indent_mode.h
@@ -0,0 +1,9 @@
+{
+  int this_is_two_space;
+    int this_is_four_space;
+        int this_is_eigth_space;        
+	int this_is_a_tab;
+  int this_is_two_space;
+    int this_is_four_space;
+        int this_is_eigth_space;        
+}
